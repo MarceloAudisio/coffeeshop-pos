8f6d3f561599ddc3182b9dd84ed9bf182fe34714
Hilet Aulas <brandoncarabajal@gmail.com>
2024-10-08 10:02:53 -0300

    Modelo clientes
    primer commit modelo clientes

---
+++ application/models/Clientes_model.php
@@ -0,0 +1,30 @@
+<?php
+class Clientes_model extends CI_Model {
+    public $tabla="clientes";
+    public $pk= "clientes.cliente_id";
+        // Método para obtener todos los registros de una tabla
+    public function listar() {
+        $query = $this->db->get($this->tabla);
+        return $query->result_array();
+    }
+    // Método para obtener un registro por ID
+    public function obtener_por_id($id) {
+        $query = $this->db->get_where($this->tabla, array($this->pk => $id));
+        return $query->row_array();
+    }
+    // Método para insertar un nuevo registro
+    public function nuevo($datos) {
+        return $this->db->insert($this->tabla, $datos);
+    }
+    // Método para actualizar un registro existente
+    public function update($id, $datos) {
+        $this->db->where($this->pk, $id);
+        return $this->db->update($this->tabla, $datos);
+    }
+
+    // Método para eliminar un registro
+    public function delete($id) {
+        $this->db->where($this->pk, $id);
+        return $this->db->delete($this->tabla);
+    }
+}
